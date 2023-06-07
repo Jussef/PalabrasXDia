b35c8fcd7fca4ddc9655a5153d61995600ae1983
Daniel Jussef <47371363+Jussef@users.noreply.github.com>
2023-06-06 23:55:25 -0600

    funciones

--- public/main.js
+++ public/main.js
@@ -1,257 +1,25 @@
-let data = [
+// Salto de linea  -  \r\nEjemplo de uso:",
+
+const data = [
   {
-    PALABRAS: "INEXORABLE",
-    DEFINICIÓN: "Que no se puede evitar, eludir o detener.\r\nEjemplo de uso: El inexorable paso del tiempo.",
-    "NUMERACIÓN DÍA": "1",
+    palabra: "INEXORABLE",
+    definicion: "Que no se puede evitar, eludir o detener.",
+    numero: "1",
+    uso: "El inexorable paso del tiempo.",
   },
   {
-    PALABRAS: "AUTOPIESIS",
-    DEFINICIÓN: "Concepto asociado a que todo ser vivo se encuentra dentro de un sistema cerrado que constantemente está creciendo y creándose a si mismo. Es propuesto por el científico chileno Humberto Maturana.",
-    "NUMERACIÓN DÍA": "2",
+    palabra: "AUTOPIESIS",
+    definicion: "Concepto asociado a que todo ser vivo se encuentra dentro de un sistema cerrado que constantemente está creciendo y creándose a si mismo. Es propuesto por el científico chileno Humberto Maturana.",
+    numero: "2",
+    uso: "Un bonito recuerdo es inmarcesible.",
   },
   {
-    PALABRAS: "INMARCESIBLE",
-    DEFINICIÓN: "Que no se puede marchitar, inmarchitable.\r\nEjemplo de uso: Un bonito recuerdo es inmarcesible.",
-    "NUMERACIÓN DÍA": "3",
-  },
-  {
-    PALABRAS: "JIBARIZAR",
-    DEFINICIÓN: "Reducir el tamaño, la cantidad o la importancia de una cosa.\r\nEjemplo de uso: El gobierno jibarizó las subvenciones públicas a diversos museos",
-    "NUMERACIÓN DÍA": "4",
-  },
-  {
-    PALABRAS: "INCONMESURABLE",
-    DEFINICIÓN: "Enorme, que por su gran magnitud no puede medirse.\r\nEjemplo de uso: Tengo una colección de un valor sentimental inconmesurable.",
-    "NUMERACIÓN DÍA": "5",
-  },
-  {
-    PALABRAS: "BALDRAGAS",
-    DEFINICIÓN: "Hombre débil, sin fuerza física ni energía.",
-    "NUMERACIÓN DÍA": "6",
-  },
-  {
-    PALABRAS: "DROMOMANÍA",
-    DEFINICIÓN: "Inclinación excesiva u obsesión patológica por trasladarse de un lugar a otro.",
-    "NUMERACIÓN DÍA": "7",
-  },
-  {
-    PALABRAS: "INASIBLE",
-    DEFINICIÓN: "Que no se puede tomar con la mano.",
-    "NUMERACIÓN DÍA": "8",
-  },
-  {
-    PALABRAS: "PERENNE",
-    DEFINICIÓN: "Continuo, incesante, que no tiene intermisión.",
-    "NUMERACIÓN DÍA": "9",
-  },
-  {
-    PALABRAS: "FOTÓN",
-    DEFINICIÓN: "Cada una de las partículas que, según la física cuántica, constituyen la luz y, en general, la radiación electromagnética.",
-    "NUMERACIÓN DÍA": "10",
-  },
-  {
-    PALABRAS: "COLEGIR",
-    DEFINICIÓN: "Sacar una conclusión por medio de un razonamiento a partir de hechos, indicios, supuestos anteriores o de un principio general.",
-    "NUMERACIÓN DÍA": "11",
-  },
-  {
-    PALABRAS: "ATIZAR",
-    DEFINICIÓN: "1. Remover o alimentar el fuego para que arda más.\r\n\r\n2. Hacer más fuerte o intenso un sentimiento o una pasión.",
-    "NUMERACIÓN DÍA": "12",
-  },
-  {
-    PALABRAS: "ENCOMIAR",
-    DEFINICIÓN: "Alabar con encarecimiento a alguien o algo.",
-    "NUMERACIÓN DÍA": "13",
-  },
-  {
-    PALABRAS: "EXÓGENO",
-    DEFINICIÓN: "De origen externo.",
-    "NUMERACIÓN DÍA": "14",
-  },
-  {
-    PALABRAS: "PULULAR",
-    DEFINICIÓN: "Abundar y moverse mucho en un lugar [personas, animales o cosas].",
-    "NUMERACIÓN DÍA": "15",
-  },
-  {
-    PALABRAS: "INEFABLE",
-    DEFINICIÓN: "Que no puede ser dicho, explicado o descrito con palabras, generalmente por tener cualidades excelsas o por ser muy sutil o difuso.",
-    "NUMERACIÓN DÍA": "16",
-  },
-  {
-    PALABRAS: "AFLAUTADO, AFLAUTADA",
-    DEFINICIÓN: "Sonido agudo y suave, como el de la flauta.",
-    "NUMERACIÓN DÍA": "17",
-  },
-  {
-    PALABRAS: "VELEIDOSO, VELEIDOSA",
-    DEFINICIÓN: "Que es poco constante en sus actos, gustos o ideas.",
-    "NUMERACIÓN DÍA": "18",
-  },
-  {
-    PALABRAS: "MUDABLE",
-    DEFINICIÓN: "Persona que muda con facilidad de opinión, gustos, etc.",
-    "NUMERACIÓN DÍA": "19",
-  },
-  {
-    PALABRAS: "CULTIPICAÑO",
-    DEFINICIÓN: "Persona culta y picaresca conjuntamente.",
-    "NUMERACIÓN DÍA": "20",
-  },
-  {
-    PALABRAS: "HOMBRUNO,HOMBRUNA",
-    DEFINICIÓN: "Dicho de una mujer: Que por alguna cualidad o circunstancia se parece a un hombre.",
-    "NUMERACIÓN DÍA": "21",
-  },
-  {
-    PALABRAS: "ESTÓLIDO",
-    DEFINICIÓN: "Falto de razón y discurso.",
-    "NUMERACIÓN DÍA": "22",
-  },
-  {
-    PALABRAS: "INOBJETABLE",
-    DEFINICIÓN: "Que no puede ser objetado o recibir objeciones.",
-    "NUMERACIÓN DÍA": "23",
-  },
-  {
-    PALABRAS: "FLÉBIL ",
-    DEFINICIÓN: "1. Digno de ser llorado.\r\n\r\n2. Lamentable, triste, lacrimoso.",
-    "NUMERACIÓN DÍA": "24",
-  },
-  {
-    PALABRAS: "SÓRDIDO",
-    DEFINICIÓN: "1. Que es miserable o sucio\r\n\r\n2. Que es indecente o malicioso.",
-    "NUMERACIÓN DÍA": "25",
-  },
-  {
-    PALABRAS: "DELEZNABLE ",
-    DEFINICIÓN: "Que es despreciable y no merece ser tenido en consideración.",
-    "NUMERACIÓN DÍA": "26",
-  },
-  {
-    PALABRAS: "CHABACANO",
-    DEFINICIÓN: "Que es grosero, ordinario o de mal gusto.",
-    "NUMERACIÓN DÍA": "27",
-  },
-  {
-    PALABRAS: "SUSPICAZ",
-    DEFINICIÓN: "Propenso a concebir sospechas o a tener desconfianza.",
-    "NUMERACIÓN DÍA": "28",
-  },
-  {
-    PALABRAS: "MONOMANÍA",
-    DEFINICIÓN: "Locura o delirio parcial sobre una sola idea o un solo orden de ideas.",
-    "NUMERACIÓN DÍA": "29",
-  },
-  {
-    PALABRAS: "ARDID",
-    DEFINICIÓN: "Acción hábil con que se pretende engañar a alguien o conseguir algo.",
-    "NUMERACIÓN DÍA": "30",
-  },
-  {
-    PALABRAS: "ALIPORI",
-    DEFINICIÓN: 'Verguenza ajena. Concepto oficial de lo coloquialmente llamando "cringe"',
-    "NUMERACIÓN DÍA": "31",
-  },
-  {
-    PALABRAS: "INTRINCAR",
-    DEFINICIÓN: "1. Enredar o enmarañar una cosa.\r\n\r\n2. Confundir los pensamientos o conceptos.",
-    "NUMERACIÓN DÍA": "32",
-  },
-  {
-    PALABRAS: "FASTUOSO",
-    DEFINICIÓN: "Que causa admiración por su grandeza, lujo o perfección.",
-    "NUMERACIÓN DÍA": "33",
-  },
-  {
-    PALABRAS: "FATUO ",
-    DEFINICIÓN: "[persona] Que se muestra engreído en su actitud, comportamiento o manera de hablar.",
-    "NUMERACIÓN DÍA": "34",
-  },
-  {
-    PALABRAS: "MISÁNTROPO",
-    DEFINICIÓN: "Persona que huye del trato con otras personas, sientiendo gran aversión o rechazo hacia ellas.",
-    "NUMERACIÓN DÍA": "35",
-  },
-  {
-    PALABRAS: "SICOFANTE,SICOFANTA",
-    DEFINICIÓN: "Impostor, calumniador. Busca obtener una posición o estatus personal mediante adulación hacia otras personas que comúnmente disponen ya de ciertas influencias y estatus social.",
-    "NUMERACIÓN DÍA": "36",
-  },
-  {
-    PALABRAS: "ESTRÉPITO",
-    DEFINICIÓN: "1. Ruido considerable.\r\n\r\n2.Ostentación, aparato en la realización de algo.",
-    "NUMERACIÓN DÍA": "37",
-  },
-  {
-    PALABRAS: "TORPEDEAR",
-    DEFINICIÓN: "Hacer fracasar un asunto o un proyecto.",
-    "NUMERACIÓN DÍA": "38",
-  },
-  {
-    PALABRAS: "JERARCA",
-    DEFINICIÓN: "Persona que tiene elevada categoría en una organización, una empresa, etc.",
-    "NUMERACIÓN DÍA": "39",
-  },
-  {
-    PALABRAS: "ESOTÉRICO",
-    DEFINICIÓN: "1. Oculto, reservado.\r\n\r\n2. Dicho de una cosa: Que es impenetrable o de difícil acceso para la mente.",
-    "NUMERACIÓN DÍA": "40",
-  },
-  {
-    PALABRAS: "EXOTÉRICO",
-    DEFINICIÓN: "Común, accesible para el vulgo, en oposición a esotérico.",
-    "NUMERACIÓN DÍA": "41",
-  },
-  {
-    PALABRAS: "BRAVATA",
-    DEFINICIÓN: "Amenaza proferida con arrogancia para intimidar a alguien.",
-    "NUMERACIÓN DÍA": "42",
-  },
-  {
-    PALABRAS: "LABRIEGO,LABRIEGA",
-    DEFINICIÓN: "Persona que tiene por oficio trabajar y cultivar la tierra y vive en el medio rural.",
-    "NUMERACIÓN DÍA": "43",
-  },
-  {
-    PALABRAS: "SOCARRONERÍA",
-    DEFINICIÓN: "Modo de expresarse de la persona que se burla de los demás de manera irónica y con apariencia de ingenuidad.",
-    "NUMERACIÓN DÍA": "44",
-  },
-  {
-    PALABRAS: "INVERECUNDO,INVERECUNDA",
-    DEFINICIÓN: "Desvergonzado, que no tiene vergüenza.",
-    "NUMERACIÓN DÍA": "45",
-  },
-  {
-    PALABRAS: "GRISÁCEO",
-    DEFINICIÓN: "Que tiene un tono gris.",
-    "NUMERACIÓN DÍA": "46",
-  },
-  {
-    PALABRAS: "ESCUETO",
-    DEFINICIÓN: "Que es simple, sin adornos, detalles superfluos ni cosas innecesarias.",
-    "NUMERACIÓN DÍA": "47",
-  },
-  {
-    PALABRAS: "ABSTRUSO",
-    DEFINICIÓN: "De difícil comprensión.",
-    "NUMERACIÓN DÍA": "48",
-  },
-  {
-    PALABRAS: "ECUÁNIME",
-    DEFINICIÓN: "Que obra con rectitud, justicia e imparcialidad o está dotado de ellas",
-    "NUMERACIÓN DÍA": "49",
-  },
-  {
-    PALABRAS: "AMAINAR",
-    DEFINICIÓN: "1. Dicho del viento: Aflojar, perder su fuerza.\r\n\r\n2. Aflojar o ceder en algún deseo, empeño o pasión.",
-    "NUMERACIÓN DÍA": "50",
+    palabra: "INMARCESIBLE",
+    definicion: "Que no se puede marchitar, inmarchitable.\r\nEjemplo de uso: Un bonito recuerdo es inmarcesible.",
+    numero: "3",
+    uso: "Un bonito recuerdo es inmarcesible.",
   },
 ];
-
-// fin json
 
 function getDiaOfYear() {
   const now = new Date();
@@ -266,17 +34,16 @@ function getDiaOfYear() {
 // Ponemos en una variable lo que salga de la funcion
 const dayOfYear = getDiaOfYear();
 
-// Ponemos en constantes la palabra y su definición
+// Ponemos en constantes la palabra y su definicion
 const restaDia = dayOfYear - 157;
-const palabraData = data[restaDia].PALABRAS;
-const definicionData = data[restaDia].DEFINICIÓN;
+const palabraData = data[restaDia].palabra;
+const definicionData = data[restaDia].definicion;
+const usoData = data[restaDia].uso;
 
 // Actualizar el contenido del elemento HTML
-const dayOfYearElement = document.getElementById("dayOfYear");
 const palabraElement = document.getElementById("palabra");
 const defElement = document.getElementById("definicion");
-dayOfYearElement.textContent = "Hoy es el día " + dayOfYear + " del año.";
-palabraElement.textContent = "La palabra del día es: " + palabraData;
-defElement.textContent = "Definición: " + definicionData;
-
-console.log(palabraData);
+const usoElement = document.getElementById("uso");
+palabraElement.textContent = "" + palabraData;
+defElement.textContent = "" + definicionData;
+usoElement.textContent = "Ejemplo de uso: " + usoData;
